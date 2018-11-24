04d9677a92c64800c229f42ae56f015085fc463b
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-24 04:12:20 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  class Pirate
+
+    attr_accessor :name, :type
+
+    SHIPS = []
+
+    def initialize(params)
+      @name= params[:name]
+      @weight= params[:type]
+      SHIPS << self
+    end
+
+    def self.all
+      SHIPS
+    end
+
+
+  end
 end
