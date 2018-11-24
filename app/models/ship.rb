abc80a85601d62e8b2eba852851fb0b57af772ac
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-24 04:29:43 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,13 +1,14 @@
 class Ship
   class Pirate
 
-    attr_accessor :name, :type
+    attr_accessor :name, :type, :booty
 
     SHIPS = []
 
     def initialize(params)
       @name= params[:name]
       @weight= params[:type]
+      @booty= params[:booty]
       SHIPS << self
     end
 
