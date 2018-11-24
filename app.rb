8ba3f4e7c604566304eedfc374c9bb8148e9a731
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-24 04:44:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,8 +16,8 @@ module FormsLab
   post '/pirates' do
     @pirate = Pirate.new(params[:pirate])
 
-    params[:pirate][:ships].each do |ship|
-      Ship.new(ship)
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
     end
     @ships= Ship.all
 
